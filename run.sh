e58fd39a1b618d8dee4dc0c8e629b730a2d35b3f
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2022-05-03 19:18:46 +0300

    Update exhaustive search

--- run.sh
+++ run.sh
@@ -1,10 +1,35 @@
-#!/usr/bin/env bash
+#!/bin/sh
+# Wrapper script for docker.
+#
+# This is used primarily for wrapping the GNU Make workflow.
+# Instead of typing "make TARGET", type "./run.sh make TARGET".
+# This will run the make workflow within a docker container.
+#
+# The assumption is that you are working in the src/ontology folder;
+# we therefore map the whole repo (../..) to a docker volume.
+#
+# See README-editors.md for more details.
 
-set -e
+IMAGE=${IMAGE:-monarchinitiative/mapping-commons}
+MEMORY=${MEMORY:-20G}
 
-IM=monarchinitiative/mapping-commons
+ODK_DEBUG=${ODK_DEBUG:-no}
 
-docker run -e ROBOT_JAVA_ARGS='-Xmx56G' \
-  -e JAVA_OPTS='-Xmx56G' \
-  -v $PWD/:/work \
-  -w /work --rm -ti $IM "$@"
+TIMECMD=
+if [ x$ODK_DEBUG = xyes ]; then
+    # If you wish to change the format string, take care of using
+    # non-breaking spaces (U+00A0) instead of normal spaces, to
+    # prevent the shell from tokenizing the format string.
+    TIMECMD="/usr/bin/time -f ### DEBUG STATS ###\nElapsed time: %E\nPeak memory: %M kb"
+fi
+
+echo "Running Process with Memory $MEMORY and Image $IMAGE"
+docker run -v $PWD/:/work -w /work -e ROBOT_JAVA_ARGS="-Xmx${MEMORY}" -e JAVA_OPTS="-Xmx${MEMORY}" --rm -ti $IMAGE $TIMECMD "$@"
+
+
+
+
+
+
+
+
