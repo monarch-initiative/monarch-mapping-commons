9301571f6077d4fde0e3e6f9e9891145d45d7e47
glass-ships <glass.ships@outlook.com>
2023-04-20 13:44:42 -0600

    run_build / make all fails cuz several sssom.tsv files are not available at links in registry.yaml

--- run_build.sh
+++ run_build.sh
@@ -10,6 +10,9 @@ VERSION=0.1.0
 sh update_registry.sh
 make -f docker.Makefile build
 
+mkdir -p tmp
 sh run.sh make IM=$IM MEMORY=$MEMORY mappings
 sh run.sh make IM=$IM MEMORY=$MEMORY all
+# rm -rf tmp
+
 echo "The Mapping Commons run has been successfully completed."
